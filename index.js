b065f7200c2ccb929a82d1856136aad782aef9b4
Tim <128478437+timcharlier6@users.noreply.github.com>
2024-04-24 19:10:39 +0200

    update

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer  = require('multer');
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Configure multer for handling file uploads
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), function (req, res) {
+  // Extract file information
+  const fileInfo = {
+    filename: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size // Size in bytes
+  };
+
+  // Respond with file information in JSON format
+  res.json(fileInfo);
+});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
+
