8f9adc1646298eeeb6eea5a5ec76250d8d12fc7d
Tim <128478437+timcharlier6@users.noreply.github.com>
2024-04-24 19:15:33 +0200

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
-app.post('/fileanalyse', upload.single('file'), function (req, res) {
+app.post('/api/fileanalyse', upload.single('file'), function (req, res) {
   // Extract file information
   const fileInfo = {
     filename: req.file.originalname,
